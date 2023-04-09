5da37407d553c2a9f834a08ebbd200deadb42298
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2023-04-09 10:45:35 +0800

    Initial Commit

---
+++ Task-Management/Task_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagementApp.swift
+//  Task-Management
+//
+//  Created by NICOLE TAN YITONG stu on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
